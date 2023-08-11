0d551a4c15e73a5dcb8ceeab915839c69f0153e5
ZeroDelusions <dmytro.koso@gmail.com>
2023-08-11 11:08:35 +0200

    Fixed

--- Sources/CImple/ResultBuilders.swift
+++ Sources/CImple/ResultBuilders.swift
@@ -2,16 +2,16 @@ import Foundation
 import CoreImage
 
 @resultBuilder
-internal struct FilterBuilder {
-    internal static func buildBlock(_ components: [CIFilter]...) -> [CIFilter] {
+public struct FilterBuilder {
+    public static func buildBlock(_ components: [CIFilter]...) -> [CIFilter] {
         components.flatMap { $0 }
     }
     
-    internal static func buildExpression(_ expression: CIFilter) -> [CIFilter] {
+    public static func buildExpression(_ expression: CIFilter) -> [CIFilter] {
         [expression]
     }
     
-    internal static func buildExpression(_ expression: [CIFilter]) -> [CIFilter] {
+    public static func buildExpression(_ expression: [CIFilter]) -> [CIFilter] {
         expression
     }
 }
