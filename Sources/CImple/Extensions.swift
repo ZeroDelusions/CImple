311b8475e6ad812f0cc0d3992daff4901239d885
ZeroDelusions <dmytro.koso@gmail.com>
2023-08-11 17:16:34 +0200

    Fix

--- Sources/CImple/Extensions.swift
+++ Sources/CImple/Extensions.swift
@@ -28,6 +28,13 @@ extension Array: FilterConvertible where Element == CIFilter {
 @available(iOS 13.0, *)
 extension Image: ImageConvertible {
     public var ciImage: CIImage? { return self.asCIImage() }
+    public func CIApply( @FilterBuilder _ filterClosure: () throws -> Any? ) rethrows -> UIImage? {
+        do {
+            return try CImple().apply(self.ciImage, try filterClosure() as! () throws -> Any?)
+        } catch {
+            return nil
+        }
+    }
 }
 
 @available(iOS 13.0, *)
@@ -72,47 +79,8 @@ extension UIView {
 }
 
 @available(iOS 13.0, *)
-extension ImageConvertible {
-    public func CIApply(_ input: ImageConvertible? = nil, @FilterBuilder _ instructions: () throws -> Any?) rethrows -> UIImage? {
-        do {
-            let result = try instructions()
-
-            let filteredImage: CIImage = try {
-                if let filters = result as? [CIFilter] {
-                    if filters.isEmpty {
-                        throw FilterError.missingReturn
-                    }
-                    guard let appliedImage = CImple().applyFilters(input?.ciImage, filters: filters) else {
-                        throw FilterError.missingInput
-                    }
-                    return appliedImage
-                } else if let ciImage = result as? CIImage {
-                    if input != nil {
-                        print("Caution: The input within FilterKit() is disregarded when using chaining syntax.")
-                    }
-                    return ciImage
-                } else if result == nil {
-                    throw FilterError.missingFilterInput
-                } else {
-                    throw FilterError.unknownError
-                }
-            }()
-
-            guard let cgImage = CIContext(options: nil).createCGImage(filteredImage, from: filteredImage.extent) else {
-                throw FilterError.renderingError
-            }
-
-            return UIImage(cgImage: cgImage)
-            
-        } catch let error as FilterError {
-            let errorDescription = "Error: \(error.description)"
-            print(errorDescription)
-            return ErrorView(errorMessage: errorDescription).asUIImage()
-        } catch {
-            print("\(error): Unknown error")
-            return nil
-        }
-    }
+extension Image {
+    
 }
 
 @available(iOS 13.0, *)
